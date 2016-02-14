dcf68bca3d5b2c6d7091aec899a47b106124c21d
WuJunxian <Axeisghost@WuJunxians-MacBook-Pro.local>
2016-02-14 17:05:21 -0500

    first

---
+++ searchPath.py
@@ -0,0 +1,60 @@
+import csv
+
+adjacentList = {}
+positionList = {}
+edgeVisitedList = {}
+path = [];
+queue = [];
+with open('CS3630_Lab2_Map1.csv', 'r') as csvfile:
+	myReader = csv.reader(csvfile, delimiter = ',')
+	for row in myReader:
+		if (len(row) == 3):
+			positionList[row[0]] = (float(row[1]), float(row[2]));
+			adjacentList[row[0]] = [];
+		if (len(row) == 2):
+			adjacentList[row[0]].append(row[1]);
+			adjacentList[row[1]].append(row[0]);
+			edgeVisitedList[(row[0], row[1])] = False;
+startNode = adjacentList.keys()[0];
+for node in adjacentList:
+	if (len(adjacentList[node]) % 2 != 0):
+		startNode = node
+		break
+prev = None
+queue.append((None, node))
+path = []
+insertPos = 0;
+while (len(queue) != 0):
+	curr = queue.pop()
+	visited = False
+	if edgeVisitedList.has_key(curr):
+		visited = edgeVisitedList[curr]
+		edgeVisitedList[curr] = True
+	elif edgeVisitedList.has_key((curr[1], curr[0])):
+		visited = edgeVisitedList[(curr[1], curr[0])]
+		edgeVisitedList[(curr[1], curr[0])] = True
+	if not visited:
+		for next in adjacentList[curr[1]]:
+			if edgeVisitedList.has_key((curr[1], next)):
+				if (not edgeVisitedList[(curr[1], next)]):
+					queue.append((curr[1], next))
+			else:
+				if (not edgeVisitedList[(next, curr[1])]):
+					queue.append((curr[1], next))
+		if (len(path) > 0):
+			if (path[insertPos - 1] != curr[0]):
+				for pos in range(0, len(path)):
+					if (path[pos] == curr[0]):
+						insertPos = pos + 1
+						break
+		path.insert(insertPos, curr[1])
+		insertPos = insertPos + 1
+print path
+positionPath = []
+for node in path:
+	positionPath.append(positionList[node])
+print positionPath
+
+
+
+
