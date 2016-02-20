c9a9210cd103251bec526c36a255488104f8f219
mli <li.menghang@gatech.edu>
2016-02-20 11:21:56 -0500

    add script lab2.py  for movement

---
+++ lab2.py
@@ -0,0 +1,68 @@
+from myro import *
+import math
+#import searchPath.py
+init("/dev/tty.Fluke2-0AC8-Fluke2")
+
+vertex = ['A', 'D', 'E', 'C', 'D', 'B', 'C', 'A', 'B']
+vertexCoor = [(0.0, 0.0), (2.0, 2.0), (1.0, 3.0), (0.0, 2.0), (2.0, 2.0), (2.0, 0.0), (0.0, 2.0), (0.0, 0.0), (2.0, 0.0)]
+
+
+
+
+
+# move from v to _v
+def move(v, _v, alpha):
+    #first turn
+    difx = _v[0] - v[0]
+    dify = _v[1] - v[1]
+    tmp = 0
+    if difx == 0:
+        if dify > 0:
+            tmp = 90
+            angle = 90 - alpha
+        elif dify < 0:
+            tmp = -90
+            angle = -90 - alpha
+    elif dify == 0:
+        if difx > 0:
+            tmp = 0
+            angle = (360 - alpha) % 360
+        else:
+            angle = 180 - alpha
+    else:
+        angle = math.atan(dify / difx) / math.pi * 180
+        tmp = angle
+        if dify > 0 and difx < 0:
+            angle += 180
+            tmp = angle
+            angle -= alpha
+        elif dify < 0 and difx > 0:
+            angle -= alpha
+        elif dify > 0 and difx > 0:
+            angle -= alpha
+        elif dify < 0 and difx < 0:
+            angle += 180
+            tmp = angle
+            angle -= alpha
+
+
+
+    
+    print "alpha is: {}, turning angle from alpha: {}".format(alpha, angle)
+    #angle = angle % 360
+    #print "turning angle after modulation is: {}".format(angle)
+    turnBy(int(angle), "deg")
+
+    #then translate
+    l = math.sqrt(difx ** 2 + dify ** 2)
+    forward(0.5, l)
+    return tmp
+
+
+alpha = 0
+for i in range(len(vertexCoor) - 1):
+    alpha = move(vertexCoor[i], vertexCoor[i + 1], alpha)
+
+# a = move((0.0, 0.0), (2.0, 2.0), 0)
+# a = move((2.0, 2.0), (1.0, 3.0), a)
+# a = move((1.0, 3.0), (0.0, 2.0), a)
